b0f4794fb57706e878bfccd5cd08f4d59b093ca6
Cwilliam97 <wcaicedom@unicauca.edu.co>
2023-02-01 17:13:06 -0500

    Actualizacion_Vista_JFrame_Login_Registro_Administrador

--- proyecto_maven_administrador/src/main/java/servicios/AdministradorServices.java
+++ proyecto_maven_administrador/src/main/java/servicios/AdministradorServices.java
@@ -7,6 +7,7 @@ package servicios;
 import java.util.List;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
@@ -14,6 +15,7 @@ import javax.ws.rs.core.MediaType;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 import models.Administrador;
+
 /**
  *
  * @author William
@@ -27,31 +29,44 @@ public class AdministradorServices {
         this.endPoint = "http://localhost:5000/api/administradores";
         this.objClientePeticiones = ClientBuilder.newClient().register(new JacksonFeature());
     }
-    
-    	public Administrador consultarAdministradorByInicioSesion(String login, String contrasenia)
-	{
-		Administrador  objAdministrador=null;	
-		
-		WebTarget target = this.objClientePeticiones.target(this.endPoint+"/"+login+"/"+contrasenia);
-		
-		Invocation.Builder objPeticion=target.request(MediaType.APPLICATION_JSON_TYPE);	
-		
-		objAdministrador = objPeticion.get(Administrador.class);
-		
-		return objAdministrador;
-	}
-        
-        public List<Administrador> listarAdministradores()
-	{
-		List<Administrador> listaAdministradores=null;			
-		
-		WebTarget target = this.objClientePeticiones.target(this.endPoint);
-		
-		Invocation.Builder objPeticion=target.request(MediaType.APPLICATION_JSON);
-		
-		listaAdministradores = objPeticion.get(new GenericType<List<Administrador>>() {});	
-		
-		return listaAdministradores;
-	}
-        
+
+    public Administrador consultarAdministradorByInicioSesion(String login, String contrasenia) {
+        Administrador objAdministrador = null;
+
+        WebTarget target = this.objClientePeticiones.target(this.endPoint + "/" + login + "/" + contrasenia);
+
+        Invocation.Builder objPeticion = target.request(MediaType.APPLICATION_JSON_TYPE);
+
+        objAdministrador = objPeticion.get(Administrador.class);
+
+        return objAdministrador;
+    }
+
+    public List<Administrador> listarAdministradores() {
+        List<Administrador> listaAdministradores = null;
+
+        WebTarget target = this.objClientePeticiones.target(this.endPoint);
+
+        Invocation.Builder objPeticion = target.request(MediaType.APPLICATION_JSON);
+
+        listaAdministradores = objPeticion.get(new GenericType<List<Administrador>>() {
+        });
+
+        return listaAdministradores;
+    }
+
+    public Administrador registrarAdministrador(Administrador objAdministradorRegistar) {
+        Administrador objAdministrador = null;
+
+        WebTarget target = this.objClientePeticiones.target(this.endPoint);
+
+        Entity<Administrador> data = Entity.entity(objAdministradorRegistar, MediaType.APPLICATION_JSON_TYPE);
+
+        Invocation.Builder objPeticion = target.request(MediaType.APPLICATION_JSON_TYPE);
+
+        objAdministrador = objPeticion.post(data, Administrador.class);
+
+        return objAdministrador;
+    }
+
 }
