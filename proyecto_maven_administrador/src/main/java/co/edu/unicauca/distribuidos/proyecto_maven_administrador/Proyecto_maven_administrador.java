9024abb781d1d7f85795fff6f7e6a0321b4980a0
Cwilliam97 <wcaicedom@unicauca.edu.co>
2023-02-01 09:04:56 -0500

    Creacion_Proyecto_Maven_Administrador

---
+++ proyecto_maven_administrador/src/main/java/co/edu/unicauca/distribuidos/proyecto_maven_administrador/Proyecto_maven_administrador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.unicauca.distribuidos.proyecto_maven_administrador;
+
+/**
+ *
+ * @author William
+ */
+public class Proyecto_maven_administrador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
